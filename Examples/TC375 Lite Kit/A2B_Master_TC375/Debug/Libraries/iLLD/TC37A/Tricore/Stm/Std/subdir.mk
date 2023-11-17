7b5c0d6b6548c90737b9f11371058ce970c411f4
Noll Markus (ATV SMD EMEA TM C) <Markus.Noll@infineon.com>
2023-11-17 13:59:49 +0100

    Fixed configuration issue in ADAU1761 codec (BCLK polarity was wrong) and therefore leading to random signal-loss on headphone. Updated SigmaStudio Configuration-Files and TC375 A2B code-examples (TC334 A2B examples not updated yet)

--- Examples/TC375 Lite Kit/A2B_Master_TC375/Debug/Libraries/iLLD/TC37A/Tricore/Stm/Std/subdir.mk
+++ Examples/TC375 Lite Kit/A2B_Master_TC375/Debug/Libraries/iLLD/TC37A/Tricore/Stm/Std/subdir.mk
@@ -20,7 +20,7 @@ C_DEPS += \
 Libraries/iLLD/TC37A/Tricore/Stm/Std/%.src: ../Libraries/iLLD/TC37A/Tricore/Stm/Std/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: TASKING C/C++ Compiler'
-	cctc -D__CPU__=tc37x "-fC:/Users/Noll/AudioKitProjects/Aurix_AudioAppKit/Examples/TC375 Lite Kit/A2B_Master_TC375/Debug/TASKING_C_C___Compiler-Include_paths.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc37x -o "$@"  "$<"  -cs --dep-file="$(@:.src=.d)" --misrac-version=2012 -N0 -Z0 -Y0 2>&1;
+	cctc -D__CPU__=tc37x "-fC:/Users/Noll/git/Aurix_AudioAppKit/Examples/TC375 Lite Kit/A2B_Master_TC375/Debug/TASKING_C_C___Compiler-Include_paths.opt" --iso=99 --c++14 --language=+volatile --exceptions --anachronisms --fp-model=3 -O0 --tradeoff=4 --compact-max-size=200 -g -Wc-w544 -Wc-w557 -Ctc37x -o "$@"  "$<"  -cs --dep-file="$(@:.src=.d)" --misrac-version=2012 -N0 -Z0 -Y0 2>&1;
 	@echo 'Finished building: $<'
 	@echo ' '
 
